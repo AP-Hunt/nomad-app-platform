88533c69523bd95236ed196c66e1ba8a7c873205
Andy Hunt <git@andyhunt.me>
2021-09-23 19:44:30 +0100

    New F# project for the API
    Creates a new DotNet Core 5.0 project in F#. It has Suave installed with
    the minimal boiler plate to ensure things are compiling and running.

---
+++ src/api/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+open System.Threading
+open Suave
+
+[<EntryPoint>]
+let main argv =
+    let cts = new CancellationTokenSource()
+    let conf = { defaultConfig with cancellationToken = cts.Token }
+    let _, server = startWebServerAsync conf (Successful.OK "Hello World")
+    
+    Async.Start(server, cts.Token)
+    printfn "Server has started"
+    Console.ReadKey true |> ignore
+    cts.Cancel()
+    
+    0
