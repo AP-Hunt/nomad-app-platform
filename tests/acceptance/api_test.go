c543e6514b2fb557f24feac51ffeaa62f8280610
Andy Hunt <git@andyhunt.me>
2021-09-30 17:38:04 +0100

    Add acceptance test for source archive -> container image
    The acceptance test submits a source archive to the API, and waits to
    see that it created a container image from it in the target registry.

---
+++ tests/acceptance/api_test.go
@@ -0,0 +1,161 @@
+package acceptance_test
+
+import (
+	"archive/zip"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"path"
+	"strconv"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"bytes"
+	"mime/multipart"
+	"net/http"
+	"os"
+
+	"github.com/heroku/docker-registry-client/registry"
+)
+
+func createZipArchive(sourceFilesPath string, fileName string) (*os.File, error) {
+	workingDir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	archivePath := path.Join(workingDir, "testdata", fileName)
+	archive, err := os.Create(archivePath)
+	if err != nil {
+		return nil, err
+	}
+	defer archive.Close()
+
+	zipWriter := zip.NewWriter(archive)
+
+	dirContent, err := ioutil.ReadDir(sourceFilesPath)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, fileInfo := range dirContent {
+		filePath := path.Join(sourceFilesPath, fileInfo.Name())
+
+		fileStream, err := os.Open(filePath)
+		if err != nil {
+			return nil, err
+		}
+		defer fileStream.Close()
+
+		zipEntry, err := zipWriter.Create(fileInfo.Name())
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = io.Copy(zipEntry, fileStream)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	zipWriter.Close()
+
+	zipFileStream, err := os.Open(archivePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return zipFileStream, nil
+}
+
+func createMultipartFormBody(archive *os.File) (*bytes.Buffer, string, error) {
+	var buffer bytes.Buffer
+	multipartWriter := multipart.NewWriter(&buffer)
+	defer multipartWriter.Close()
+
+	fileWriter, err := multipartWriter.CreateFormFile("source_bundle", archive.Name())
+	if err != nil {
+		return nil, "", err
+	}
+
+	_, err = io.Copy(fileWriter, archive)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return &buffer, multipartWriter.FormDataContentType(), nil
+}
+
+type AppResponse struct {
+	Id struct {
+		Value string
+	}
+	Name    string
+	Version int
+}
+
+var _ = FDescribe("Api", func() {
+	It("Accepts a zip file containing the source, and creates a container image from it", func() {
+		sourceArchive, err := createZipArchive("testdata/simple_go_app", "go-app.zip")
+		Expect(err).ToNot(HaveOccurred())
+		defer sourceArchive.Close()
+
+		body, contentType, err := createMultipartFormBody(sourceArchive)
+		Expect(err).ToNot(HaveOccurred())
+
+		httpClient := http.Client{}
+
+		req, err := http.NewRequest("POST", "http://localhost:5000/apps/", body)
+		Expect(err).ToNot(HaveOccurred())
+
+		req.Header.Set("Content-Type", contentType)
+
+		res, err := httpClient.Do(req)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(res.StatusCode).To(Equal(http.StatusAccepted))
+
+		appResponse := AppResponse{}
+		respBytes, err := ioutil.ReadAll(res.Body)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = json.Unmarshal(respBytes, &appResponse)
+		Expect(err).ToNot(HaveOccurred())
+
+		Eventually(func() bool {
+			hub, err := registry.New("http://localhost:6000", "", "")
+			if err != nil {
+				return false
+			}
+
+			repos, err := hub.Repositories()
+			if err != nil {
+				return false
+			}
+
+			foundRepo := false
+			for _, repo := range repos {
+				if repo == appResponse.Id.Value {
+					foundRepo = true
+					break
+				}
+			}
+
+			tags, err := hub.Tags(appResponse.Id.Value)
+			if err != nil {
+				return false
+			}
+
+			foundTag := false
+			for _, tag := range tags {
+				if tag == strconv.Itoa(appResponse.Version) {
+					foundTag = true
+					break
+				}
+			}
+
+			return foundRepo && foundTag
+		}, 5*time.Minute, 10*time.Second).Should(BeTrue())
+	})
+})
--- tests/acceptance/go.mod
+++ tests/acceptance/go.mod
@@ -5,29 +5,43 @@ go 1.17
 require (
 	github.com/hashicorp/consul/api v1.10.1
 	github.com/hashicorp/nomad/api v0.0.0-20210915002645-b7d2da3a9ebd
+	github.com/heroku/docker-registry-client v0.0.0-20190909225348-afc9e1acc3d5
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.16.0
 )
 
 require (
 	github.com/armon/go-metrics v0.0.0-20180917152333-f0300d1749da // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/docker/libtrust v0.0.0-20160708172513-aabc10ec26b7 // indirect
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/cronexpr v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/go-hclog v0.12.0 // indirect
 	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
-	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/hashicorp/serf v0.9.5 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
-	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
